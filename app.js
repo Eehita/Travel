dc4d9b187d9b69620f3bbd3d8f97bcdaaf05086f
Eehita <Eehita@pal-nat186-35-114.itap.purdue.edu>
2016-11-25 00:04:52 -0500

    testing db

--- app.js
+++ app.js
@@ -34,6 +34,21 @@ MongoClient.connect(url, function(err, db) {
   db.close();
 });
 
+var insertDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Insert some documents 
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
