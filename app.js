a0b9e5ddff0ead2fe5afe38e3bbdbd12821f2393
Eehita <Eehita@pal-nat186-35-114.itap.purdue.edu>
2016-11-24 23:58:48 -0500

    installed MongoDB, connected to microservice

--- app.js
+++ app.js
@@ -21,6 +21,19 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Connection URL 
+var url = 'mongodb://0bc2b66d-60ee-4539-8792-89d243c305da:e5fbef2c-d762-4733-b3d9-da1e3afebe63@50.23.230.149:10172/db';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+ 
+  db.close();
+});
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
